9b4bed70f7f679e17da2ad877d3839aba3df5d62
M1no <66644985+Amin-Fakia@users.noreply.github.com>
2021-10-10 13:51:31 +0200

    Better UI

--- Running_Dots.py
+++ Running_Dots.py
@@ -1,123 +1,132 @@
 import pygame
-import math  
+import math
 import random
 import time
-import pygame.gfxdraw
-from datetime import date
-from datetime import datetime
-import json
+
 pygame.init()
-
-# Get time and date
-now = datetime.now()
-dt_string = now.strftime("%d_%m_%Y_%H_%M_%S")
-
 
 # Define screen width and height
 WIDTH = 1240
 HEIGHT = 720
 display = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Running Dots')
 
 # Colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-
-
-
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 clock = pygame.time.Clock()  # To set the frame rate
-font = pygame.font.SysFont("Arial", 26) # set up font 
+font = pygame.font.SysFont("Calibri", 22)  # set up font
+font1 = pygame.font.SysFont("Calibri", 19)
+font2 = pygame.font.SysFont("Calibri", 40)
 
+cx = random.randint(110, 790)
+cy = random.randint(110, 660)
+width_of_circle = 15
 
-cx = random.randint(20, WIDTH - 20)
-cy = random.randint(20, HEIGHT - 20)
-width_of_circle = 30
-
-
-
-#Variables
+# Variables
 rts = ""
 et = 0
 tic = 0
 toc = 0
 counter = 0
 counterF = 0
-times = [] # to store the times between clicking and appearance of the dots
+times = []  # to store the times between clicking and appearance of the dots
 
-# get counter for successfull clicks
+# Get text for start screen
+def game_start():
+    return font.render("Versuche so schnell wie möglich auf den schwarzen Punkt zu klicken", True, BLACK)
+
+
+# get counter for successful clicks
 def get_counter():
-    return font.render(f"successes: {counter}",5, pygame.Color("cyan"))
+    return font.render(f"Hits: {counter}", True, GREEN)
+
+
 # get counter for failed clicks
 def get_failCounter():
-    return font.render(f"failuers: {counterF}",5, pygame.Color("red"))
+    return font.render(f"Misses: {counterF}", True, RED)
+
+
+def get_time():
+    return font.render(f"Reaktionszeit: {int(et)} ms", True, BLUE)
+
 
 # TODO:
 # both game_over and get_percentage should be one method
 # Make it so it shows percentages and numbers in a readable way
 
-# Get texts for game over screen
-def get_average():
-    sm = sum(times)/(counter) if counter > 0 else counter == 1
-    return sm
+
+# Get text for game over screen
 def game_over():
-    return font.render(f"your sucess to failure percentage is: {(counter/(counter+counterF)) *100:.2f} %",20, pygame.Color("white"))
+    return font2.render("Game Over", True, RED)
+
+
+def game_success():
+    return font1.render(f"Deine Treffer in Prozente: {(counter / (counter + counterF)) * 100:.2f} %", True, BLACK)
+
+
 def get_percentage():
-    # Try to think how we can skip a line so it looks like this
-    #                   Game Over
-    #    your sucess to failure percent is: 0.9
-    # \n doesnt work for font render we probably need to split it into 2 objects (2 font renderers)
-    
-    
-    return font.render(f"Game Over - your failure to sucess percentage is: {(counterF/(counter+counterF)) *100:.2f} % - your reaction time average: {get_average():.2f} in ms",1, pygame.Color("white"))
+    sm = sum(times) / counter if counter > 0 else counter == 1
 
-def get_time():
-    return font.render(f"Reaction Time: {int(et)} in ms",1, pygame.Color("coral"))
+    return font1.render(
+        f"Deine durchschnittliche Reaktionszeit: {sm:.2f} ms", True, BLACK)
+
+
 def start():
+    return font.render(f"{rts}", True, BLACK)
 
-    return font.render(f"{rts}",3, pygame.Color("white"))
+
 # Main loop
 while True:
-    display.fill(BLACK)
-    display.blit(get_counter(),(0,25))
-    display.blit(get_failCounter(),(0,0))
-    pygame.draw.circle(display,WHITE,  (cx, cy), width_of_circle)
+    display.fill(WHITE)
+    display.blit(game_start(), (20, 30))
+    display.blit(get_counter(), (820, 100))
+    display.blit(get_failCounter(), (820, 125))
+    pygame.draw.polygon(display, BLACK, ((20, 100), (20, 670), (800, 670), (800, 100)), 2)
+    pygame.draw.circle(display, BLACK, (cx, cy), width_of_circle)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            quit() # The reason for this is to prevent errors due to the loop going on, we need to quit the whole Python application
+            quit()  # The reason for this is to prevent errors due to the loop going on, we need to quit the whole Python application
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
-            if math.sqrt((pos[0]-cx)**2+(pos[1] - cy)**2) < width_of_circle:
+            if math.sqrt((pos[0] - cx) ** 2 + (pos[1] - cy) ** 2) < width_of_circle:
                 tic = time.time()
-                if counter > 0: # Start the time count when we on the first circle
+                if counter > 0:  # Start the time count when we click on the first circle
                     tic = toc
                 rts = "Reaction Time Test Started"
                 toc = time.time()
                 et = (toc - tic) * 1000
-                counter +=1
-                cx = random.randint(20, WIDTH - 20)
-                cy = random.randint(20, HEIGHT - 20)
+                counter += 1
+                cx = random.randint(110, 790)
+                cy = random.randint(110, 660)
                 times.append(et)
             else:
-                counterF +=1
-                
-    display.blit(get_time(),(0,50))
-    display.blit(start(),((WIDTH/2)-125,HEIGHT-35))
+                counterF += 1
+
+    display.blit(get_time(), (820, 150))
+    display.blit(start(), ((WIDTH / 2) - 125, HEIGHT - 35))
     # max numbers of tries is: 10
-    if counter > 10 or counterF > 9 or counter+counterF > 9:
+    if counter > 10 or counterF > 9 or counter + counterF > 9:
         # Load a new loop/screen
         while True:
-            display.fill(BLACK)
-            display.blit(get_percentage(),(200,HEIGHT/2))
-            display.blit(game_over(),(200,(HEIGHT/2)+30))
+            # display.fill(WHITE)
+            display.blit(game_over(), (320, (HEIGHT / 2)))
+            display.blit(game_success(), (820, (HEIGHT / 2)))
+            display.blit(get_percentage(), (820, (HEIGHT / 2) + 30))
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    with open(f"Data/{dt_string}.json", 'w') as f:
-                        json.dump({"date and time":dt_string,"average":get_average(), "failures":counterF,"successes":counter,"Success to failure percentage":counter/(counter+counterF),"failure to success percentage":counterF/(counter+counterF) },f)
                     pygame.quit()
                     quit()
             clock.tick(60)
             pygame.display.update()
-    
+
+    pygame.display.flip()
     clock.tick(60)
     pygame.display.update()
+
